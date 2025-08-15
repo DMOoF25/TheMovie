c3678b8c1bb4640de0b96a62b315d5e05267f9e4
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2025-08-15 12:26:34 +0800

    Add repository for UC1

---
+++ src/TheMovie.Application/Abstractions/IMovieRepository.cs
@@ -0,0 +1,12 @@
+﻿using TheMovie.Domain.Entities;
+
+namespace TheMovie.Application.Abstractions;
+
+public interface IMovieRepository : IRepository<Movie>
+{
+    /*
+    Task<IEnumerable<Movie>> ListByGenreAsync(Genre genre, CancellationToken cancellationToken = default);
+    Task<IEnumerable<Movie>> ListByAnyGenresAsync(Genre genres, CancellationToken cancellationToken = default);
+    Task<IEnumerable<Movie>> ListByAllGenresAsync(Genre genres, CancellationToken cancellationToken = default);
+    */
+}
