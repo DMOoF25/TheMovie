a5e0573c69552a32a6f35c159e699b07ff2f09f9
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2025-08-16 10:12:11 +0800

    Add menu items for exit and add movie Add empty AddMovieView

--- src/TheMovie.UI/MainWindow.xaml.cs
+++ src/TheMovie.UI/MainWindow.xaml.cs
@@ -1,24 +1,30 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using TheMovie.UI.Views;
 
-namespace TheMovie.UI
+namespace TheMovie.UI;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
+
+    #region Menu bar
+    private void MenuItemExit_Click(object sender, RoutedEventArgs e)
+    {
+        System.Windows.Application.Current.Shutdown();
+    }
+    private void MenuItemAddMovie_Click(object sender, RoutedEventArgs e)
+    {
+        MainFrame.Navigate(new AddMovieView());
+    }
+    private void MenuItemAbout_Click(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Show("The Movie App\nVersion 1.0\n© 2025 Team 4", "Om The Movie App", MessageBoxButton.OK, MessageBoxImage.Information);
+    }
+    #endregion
 }
---
+++ src/TheMovie.UI/Views/AddMovieView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace TheMovie.UI.Views;
+
+/// <summary>
+/// Interaction logic for AddMovie.xaml
+/// </summary>
+public partial class AddMovieView : Page
+{
+    public AddMovieView()
+    {
+        InitializeComponent();
+    }
+}
