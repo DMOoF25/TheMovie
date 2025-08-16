fe5199fdda3ff64485e7d16393f5a18a1b48240b
Jens Tirsvad Nielsen <jenstirsvad@gmail.com>
2025-08-16 11:26:07 +0800

    Add dependencyinjection

---
+++ src/TheMovie.Application/DependencyInjection.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace TheMovie.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddMovieApplication(this IServiceCollection services)
+    {
+        // Register application services / use cases here when you add them.
+        return services;
+    }
+}
